333832ef72944d7cebc789b8b3ac41e908012f8a
patriotKey <dante.camblor@gmail.com>
2021-03-08 20:41:51 -0500

    made the init and installed flask

---
+++ app.py
@@ -0,0 +1,15 @@
+"""
+# macOS/Linux
+    # You may need to run sudo apt-get install python3-venv first
+    python3 -m venv venv
+
+    # Windows
+    # You can also use py -3 -m venv .venv
+    python -m venv venv
+
+    mac / linux
+    source env/bin/activate
+
+    windows 
+    .\venv\Scripts\activate
+"""
